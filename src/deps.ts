038c701839b0700c32146261e5341da6e60f1f71
Andrew Vassilyev <xavescor@gmail.com>
2023-11-28 02:07:26 +0600

    feat(lodash): lodash packages

--- src/deps.ts
+++ src/deps.ts
@@ -30,11 +30,19 @@ export const deps = new Map<string, Dep>([
     },
   ],
   [
+    "lodash.compact",
+    {
+      minimalNodeVersion: "0.10.0",
+      nativeApi: "Array.prototype.filter & Boolean",
+      message: "you can replace lodash.compact with array.filter(Boolean)",
+    },
+  ],
+  [
     "lodash.isarray",
     {
       minimalNodeVersion: "0.10.0",
       nativeApi: "Array.isArray",
-      message: "Array.isArray(el)"
+      message: "Array.isArray(el)",
     },
   ],
   [
@@ -42,7 +50,40 @@ export const deps = new Map<string, Dep>([
     {
       minimalNodeVersion: "0.10.0",
       nativeApi: "Array.prototype.concat",
-      message: "array.concat(...arrays)"
+      message: "array.concat(...arrays)",
+    },
+  ],
+  [
+    "lodash.difference",
+    {
+      minimalNodeVersion: "0.12.0",
+      nativeApi: "Array.prototype.filter & Set",
+      message: "const set = new Set(diff); array.filter(el => !set.has(el))",
+    },
+  ],
+  [
+    "lodash.differenceby",
+    {
+      minimalNodeVersion: "0.12.0",
+      nativeApi: "Array.prototype.filter & Set",
+      message:
+        "const set = new Set(diff.map(fn)); array.filter(el => !set.has(fn(el)))",
+    },
+  ],
+  [
+    "lodash.differencewith",
+    {
+      minimalNodeVersion: "0.12.0",
+      nativeApi: "Array.prototype.filter",
+      message: "arr1.filter(el => !arr2.some(el2 => comparator(el, el2)))",
+    },
+  ],
+  [
+    "lodash.drop",
+    {
+      minimalNodeVersion: "0.10.0",
+      nativeApi: "Array.prototype.slice",
+      message: "array.slice(n)",
     },
   ],
   [
@@ -102,6 +143,62 @@ export const deps = new Map<string, Dep>([
     },
   ],
   [
+    "lodash.findindex",
+    {
+      minimalNodeVersion: "4.0.0",
+      nativeApi: "Array.prototype.findIndex",
+      message: "array.findIndex(fn)",
+    },
+  ],
+  [
+    "lodash.findlastindex",
+    {
+      minimalNodeVersion: "18.0.0",
+      nativeApi: "Array.prototype.findLastIndex",
+      message: "array.findLastIndex(fn)",
+    },
+  ],
+  [
+    "lodash.head",
+    {
+      minimalNodeVersion: "0.0.0",
+      nativeApi: "array[0]",
+      message: "array[0]",
+    },
+  ],
+  [
+    "lodash.flatten",
+    {
+      minimalNodeVersion: "11.0.0",
+      nativeApi: "Array.prototype.flat",
+      message: "array.flat()",
+    },
+  ],
+  [
+    "lodash.flattendeep",
+    {
+      minimalNodeVersion: "11.0.0",
+      nativeApi: "Array.prototype.flat",
+      message: "array.flat(Infinity)",
+    },
+  ],
+  [
+    "lodash.flattendepth",
+    {
+      minimalNodeVersion: "11.0.0",
+      nativeApi: "Array.prototype.flat",
+      message: "array.flat(depth)",
+    },
+  ],
+  [
+    "lodash.frompairs",
+    {
+      minimalNodeVersion: "12.0.0",
+      nativeApi: "Object.fromEntries",
+      message: "Object.fromEntries(iterable)",
+    },
+  ],
+  [
     "lodash.indexof",
     {
       minimalNodeVersion: "0.10.0",
@@ -115,6 +212,46 @@ export const deps = new Map<string, Dep>([
       minimalNodeVersion: "4.0.0",
       nativeApi: "Array.prototype.fill",
       message: "array.fill(value, start, end)",
+    },
+  ],
+  [
+    "lodash.initial",
+    {
+      minimalNodeVersion: "0.10.0",
+      nativeApi: "Array.prototype.slice",
+      message: "array.slice(0, -1)",
+    },
+  ],
+  [
+    "lodash.last",
+    {
+      minimalNodeVersion: "0.10.0",
+      nativeApi: "array[array.length - 1]",
+      message: "array[array.length - 1]",
+    },
+  ],
+  [
+    "lodash.nth",
+    {
+      minimalNodeVersion: "16.6.0",
+      nativeApi: "Array.prototype.at",
+      message: "array.at(index)",
+    },
+  ],
+  [
+    "lodash.pull",
+    {
+      minimalNodeVersion: "0.10.0",
+      nativeApi: "Array.prototype.filter",
+      message: "array.filter(el => !values.includes(el))",
+    },
+  ],
+  [
+    "lodash.pullall",
+    {
+      minimalNodeVersion: "0.10.0",
+      nativeApi: "Array.prototype.filter",
+      message: "array.filter(el => !values.includes(el))",
     },
   ],
 ]);
--- src/findUselessPackages.test.ts
+++ src/findUselessPackages.test.ts
@@ -3,42 +3,45 @@ import { findUselessPackages } from "./findUselessPackages.js";
 
 describe("findUselessPackages", () => {
   test("have useless deps only with node@lts", () => {
+    const deps = new Set([
+      "object-assign",
+      "object.assign",
+      "es6-object-assign",
+      "lodash.compact",
+      "lodash.isarray",
+      "lodash.concat",
+      "lodash.difference",
+      "lodash.differenceby",
+      "lodash.differencewith",
+      "lodash.drop",
+      "lodash.dropright",
+      "lodash.join",
+      "lodash.reverse",
+      "lodash.lastindexof",
+      "lodash._slice",
+      "lodash.slice",
+      "lodash.without",
+      "lodash.findindex",
+      "lodash.findlastindex",
+      "lodash.head",
+      "lodash.flatten",
+      "lodash.flattendeep",
+      "lodash.flattendepth",
+      "lodash.frompairs",
+      "lodash.indexof",
+      "lodash.fill",
+      "lodash.initial",
+      "lodash.last",
+      "lodash.nth",
+      "lodash.pull",
+      "lodash.pullall",
+    ]);
     const uselessPackages = findUselessPackages({
-      dependencies: new Set([
-        "object-assign",
-        "object.assign",
-        "es6-object-assign",
-        "lodash.isarray",
-        "lodash.dropright",
-        "lodash.join",
-        "lodash.reverse",
-        "lodash.lastindexof",
-        "lodash._slice",
-        "lodash.slice",
-        "lodash.without",
-        "lodash.indexof",
-        "lodash.fill",
-      ]),
+      dependencies: deps,
       nodeVersion: "20.99.0",
     });
 
-    expect(uselessPackages).toEqual(
-      new Set([
-        "object-assign",
-        "object.assign",
-        "es6-object-assign",
-        "lodash.isarray",
-        "lodash.dropright",
-        "lodash.join",
-        "lodash.reverse",
-        "lodash.lastindexof",
-        "lodash._slice",
-        "lodash.slice",
-        "lodash.without",
-        "lodash.indexof",
-        "lodash.fill",
-      ]),
-    );
+    expect(uselessPackages).toEqual(deps);
   });
 
   test("have mixed deps", () => {
@@ -57,5 +60,14 @@ describe("findUselessPackages", () => {
     });
 
     expect(uselessPackages).toEqual(new Set());
+  });
+
+  test("have useless package but dep node version is any", () => {
+    const uselessPackages = findUselessPackages({
+      dependencies: new Set(["lodash.head"]),
+      nodeVersion: "0.0.0",
+    });
+
+    expect(uselessPackages).toEqual(new Set(["lodash.head"]));
   });
 });
