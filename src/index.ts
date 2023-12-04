1c95b9216fc4962b9dec844fdc3e75234695ffd7
Andrew Vassilyev <xavescor@gmail.com>
2023-12-05 03:21:27 +0600

    21: fix public exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { createValidateFn } from "./config/createValidateFn.js";
 export { mergeValidateFn } from "./config/mergeValidateFn.js";
 export { declareValidation } from "./config/declareValidation.js";
 export { validDep, invalidDep } from "./config/createValidateFn.js";
+// we need to hide this function from the public API on type level
+export { run } from "./run.js";
